2a163465dd97fe195ea86b44efc4f090805bd85b
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-07-13 22:46:24 +0900

    jsonからJavaオブジェクトへ変換

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import items.Item;
+
 /**
  * @author muramasa
  *
@@ -15,8 +27,32 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try {
+			String jsonDirectory = "/Users/muramasa/ws/java/eclipse-workspace/VendingMachine/resources/";
+			
+			ObjectMapper om = new ObjectMapper();
+			
+			List<Item> foods = Arrays.asList(om.readValue(new File(jsonDirectory + "foods.json"), Item[].class));
+			List<Item> drinks = Arrays.asList(om.readValue(new File(jsonDirectory + "drinks.json"), Item[].class));
+			
+			foods.forEach(item -> {
+				System.out.println(item.getName() + " " + item.getPrice() + " " + item.getType());
+			});
+			
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
 	}
-
+	
 }
