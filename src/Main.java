6bbef7b4c8e0b5b3b682cddd6ae19879c15d36b9
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2020-07-14 23:45:48 +0900

    自販機と商品選択

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -36,22 +39,26 @@ public class Main {
 			List<Item> foods = Arrays.asList(om.readValue(new File(jsonDirectory + "foods.json"), Item[].class));
 			List<Item> drinks = Arrays.asList(om.readValue(new File(jsonDirectory + "drinks.json"), Item[].class));
 			
-			foods.forEach(item -> {
-				System.out.println(item.getName() + " " + item.getPrice() + " " + item.getType());
+//			foods.forEach((item) -> {
+//				System.out.println(item.getName() + " " + item.getPrice());
+//			});
+			IntStream.range(0, foods.size()).forEach(i -> {
+				System.out.printf("%2d: %-10s  ¥%-5d\r", 
+						i+1, foods.get(i).getName(), foods.get(i).getPrice());
 			});
+			System.out.println("購入したい商品番号を入力してください: ");
+			Scanner scan = new Scanner(System.in);
+			int input = scan.nextInt();
 			
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
+			System.out.println(foods.get(input).getName());
+		
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("存在しない商品です");
+		} catch(InputMismatchException e) {
+			System.out.println("番号で選んでください");
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
+		}
 		
 	}
 	
